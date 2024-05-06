b975edaf10f54232009b7ba3e8aba271e9ca7f28
Michał <michal04janik@gmail.com>
2024-05-06 21:20:04 +0200

    Budowa Mapy

---
+++ gra_projektNPG/player_test_SPAWN.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+#extends KinematicBody2D
+
+func _ready():
+    # Ustawienie nowej pozycji postaci
+    position = Vector2(20, 20) # zamiast x i y wpisz odpowiednie wartości współrzędnych
+
