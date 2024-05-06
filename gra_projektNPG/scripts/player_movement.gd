12185d8c87439727291718e5043e36f7ea3424cd
Seweryn Gurgul <sgurgul@student.agh.edu.pl>
2024-05-06 20:41:12 +0200

    Chnages_movement2

--- gra_projektNPG/scripts/player_movement.gd
+++ gra_projektNPG/scripts/player_movement.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -600.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var sprite = $AnimatedSprite2D
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
