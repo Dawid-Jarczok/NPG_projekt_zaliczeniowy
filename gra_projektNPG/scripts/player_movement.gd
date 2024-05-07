6663d043450c465d42da893e81a631434c7ae5e1
Seweryn Gurgul <sgurgul@student.agh.edu.pl>
2024-05-07 12:34:16 +0200

    change speed fall

--- gra_projektNPG/scripts/player_movement.gd
+++ gra_projektNPG/scripts/player_movement.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
-
+var max_fall_speed = 300
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,6 +13,8 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+	velocity.y = clamp(velocity.y, -max_fall_speed, max_fall_speed)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -26,6 +28,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	#Changing the character's direction 
 	if direction < 0:
 		sprite.flip_h = true
 	elif direction > 0:
