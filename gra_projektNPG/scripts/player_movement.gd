4c2948758cd2e1b6a0db6df7a6eb236be4313114
Dawid-Jarczok <dawid-jarczok@wp.pl>
2024-05-14 21:58:44 +0200

    Dust stays in place

--- gra_projektNPG/scripts/player_movement.gd
+++ gra_projektNPG/scripts/player_movement.gd
@@ -19,7 +19,6 @@ var current_state = State
 var collider_name = null
 var Enemies = ["Enemy_mushroom", "Enemy_mushroom2","Enemy_mushroom3","Enemy_mushroom4","Enemy_mushroom5","Enemy_mushroom6"]
 var if_was_falling: bool = true
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -29,7 +28,7 @@ var spawnPoint
 @onready var left_col_vec = $collision_left
 @onready var right_col_vec = $collision_right
 @onready var down_col_vec = $collision_down
-@onready var dust = $AnimatedSprite2D
+@onready var dust = get_node("/root/Map1/player_test/Dust")
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("TestAction"):
@@ -151,7 +150,6 @@ func who_hit_player_on_right():
 	else:
 		collider_name = null
 
-
 func _on_taked_damage_timer_timeout():
 	block_movement_inputs = false
 
@@ -166,7 +164,9 @@ func dust_after_falling():
 		if_was_falling = true
 	if down_col_vec.is_colliding():
 		if if_was_falling:
+			if_was_falling = false
+			dust.global_position = global_position + Vector2(0, 18)
 			dust.play("dust")
 			await get_tree().create_timer(0.15).timeout
 			dust.play("default")
-			if_was_falling = false
+			
