e0fec5a1eab57360963297b7b49929e21e7604e7
Dawid-Jarczok <dawid-jarczok@wp.pl>
2024-05-19 02:09:37 +0200

    Added score info on display

--- gra_projektNPG/scripts/core/GameManager.gd
+++ gra_projektNPG/scripts/core/GameManager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-signal gainded_score(int)
+signal gained_score(int)
 
 var player : Player
 
@@ -8,5 +8,5 @@ var score : int = 0
 
 func gain_score(score_gained):
 	score += score_gained
-	emit_signal("gainded_score", score_gained)
+	emit_signal("gained_score", score_gained)
 	prints("Score: ", score)
