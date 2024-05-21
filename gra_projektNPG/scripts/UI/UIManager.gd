8dd03c426871823904b4c3ec70d6bc85c8796c96
Dawid-Jarczok <dawid-jarczok@wp.pl>
2024-05-21 13:26:28 +0200

    Moved functionality of world.gd to UIManager.gd

--- gra_projektNPG/scripts/UI/UIManager.gd
+++ gra_projektNPG/scripts/UI/UIManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 func _ready():
 	GameManager.gained_score.connect(update_score_display)
+	$heartsContainer.setMaxHearts(5)
 
 func _process(delta):
 	$FPSMeter.text = "FPS: " + str(Engine.get_frames_per_second())
