bedbae19b912395c210d3fc72f8f641591c341d9
Dawid-Jarczok <dawid-jarczok@wp.pl>
2024-05-26 23:11:06 +0200

    Added hearts to collect

--- gra_projektNPG/scripts/Items/heart.gd
+++ gra_projektNPG/scripts/Items/heart.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
+var collected = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if collected:
+		#Changing transparency
+		modulate.a = lerp(modulate.a, 0.0, 2.0 * delta)
 
 
 func _on_area_2d_body_entered(body:Node2D):
-	pass # Replace with function body.
+	if body.is_in_group("Player"):
+		collected = true
+		GameManager.gain_health(1)
+		await get_tree().create_timer(0.5).timeout
+		hide()
+		queue_free()
