17dbd3109fce7bcf11f68070fcdf2c38c7c45a0d
Krzysztof Jablonski <krzjab@student.agh.edu.pl>
2024-05-27 12:48:11 +0200

    bananTest3

--- gra_projektNPG/scripts/Items/banana_collectible.gd
+++ gra_projektNPG/scripts/Items/banana_collectible.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
 var collected = false
-'''
+
+
 func _process(delta):
 	if collected:
 		#Changing transparency
-		modulate.a = lerp(modulate.a, 0.0, 2.0 * delta)
-'''
+		$AnimatedSprite2D.play("collected")
+		await get_tree().create_timer(0.25).timeout
+		queue_free()
 
-func _on_area_2d_body_entered(body:Node2D):
+func _on_banana_body_entered(body):
 	if body.is_in_group("Player"):
 		collected = true
 		GameManager.gain_score(1)
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.3).timeout
 		hide()
 		queue_free()
