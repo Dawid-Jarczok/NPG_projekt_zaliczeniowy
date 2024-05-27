ec7e04930d89209427631c709b0e61c4ab6cbddc
Krzysztof Jablonski <krzjab@student.agh.edu.pl>
2024-05-27 11:05:26 +0200

    banan_test1

---
+++ gra_projektNPG/scripts/Items/Banana_score.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var collected = false
+'''
+func _process(delta):
+	if collected:
+		#Changing transparency
+		modulate.a = lerp(modulate.a, 0.0, 2.0 * delta)
+'''
+
+func _on_area_2d_body_entered(body:Node2D):
+	if body.is_in_group("Player"):
+		collected = true
+		GameManager.gain_score(1)
+		await get_tree().create_timer(0.5).timeout
+		hide()
+		queue_free()
