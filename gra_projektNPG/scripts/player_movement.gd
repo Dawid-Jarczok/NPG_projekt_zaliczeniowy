9d1d4420367982a62a1460d28262388d91fbe8bf
Dawid-Jarczok <dawid-jarczok@wp.pl>
2024-05-30 19:02:45 +0200

    Fixed jump action detection

--- gra_projektNPG/scripts/player_movement.gd
+++ gra_projektNPG/scripts/player_movement.gd
@@ -87,7 +87,7 @@ func player_falling(delta):
 func player_jump(delta):
 	if block_movement_inputs:
 		return
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 func player_in_air(delta):
