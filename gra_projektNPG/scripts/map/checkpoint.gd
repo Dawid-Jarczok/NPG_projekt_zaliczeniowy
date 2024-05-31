e81cfad8aed4510eea766e6daa273f22f2ec026a
Dawid-Jarczok <82517250+Dawid-Jarczok@users.noreply.github.com>
2024-06-01 00:47:03 +0200

    Fixed checkpoint player spawn position

--- gra_projektNPG/scripts/map/checkpoint.gd
+++ gra_projektNPG/scripts/map/checkpoint.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 func _on_checkpoint_area_body_entered(body:Node2D):
 	if body.is_in_group("Player"):
-		GameManager.set_checkpoint(global_position)
+		GameManager.set_checkpoint(global_position + Vector2(0, -40))
